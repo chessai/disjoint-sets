04a50c9d3e46f4e32cf82219a82ead88f9c52227
chessai <chessai1996@gmail.com>
2020-04-30 22:20:44 -0700

    documentation and some pragmas

--- src/DisjointSets.hs
+++ src/DisjointSets.hs
@@ -26,39 +26,37 @@ import qualified Data.Map.Strict as Map
 import qualified Data.Vector as Vector
 import qualified Data.Vector.Mutable as MVector
 
-type Index = Int
-
+-- | 'DisjointSets' represents a set of disjoint (non-overlapping) sets.
+--
+--   In nearly-constant time, you can find the smallest element of any
+--   set given an element of that set (with 'find'), or you can combine
+--   two sets (with 'union').
+--
+--   The maximum supported size of this structure is roughly 2^32 elements.
 data DisjointSets s a = DisjointSets
-  { sizeVar :: {-# UNPACK #-} !(MutVar s Int)
-  , toIndexVar :: {-# UNPACK #-} !(MutVar s (Map a Index))
-  , fromIndexVar :: {-# UNPACK #-} !(MutVar s (Map Index a))
-  , nodesVar :: {-# UNPACK #-} !(MutVar s (MVector s Node))
+  { sizeVar :: {-# unpack #-} !(MutVar s Int)
+  , toIndexVar :: {-# unpack #-} !(MutVar s (Map a Index))
+  , fromIndexVar :: {-# unpack #-} !(MutVar s (Map Index a))
+  , nodesVar :: {-# unpack #-} !(MutVar s (MVector s Node))
   }
 
-data Node = Node
-  { parent :: Index
-  , size :: Int
-  }
-
-empty :: (PrimMonad m) => m (DisjointSets (PrimState m) a)
+-- | An 'empty' 'DisjointSets'.
+empty :: (PrimMonad m)
+  => m (DisjointSets (PrimState m) a)
 empty = DisjointSets
   <$> newMutVar 0
   <*> newMutVar Map.empty
   <*> newMutVar Map.empty
   <*> (newMutVar =<< MVector.new 1)
+{-# inline empty #-}
 
-atMaxCapacity :: PrimMonad m => DisjointSets (PrimState m) a -> m Bool
-atMaxCapacity d = go <$> readMutVar (sizeVar d) <*> (MVector.length <$> getNodes d)
-  where
-    go x y = x + 1 >= y
-
-resize :: PrimMonad m => DisjointSets (PrimState m) a -> m ()
-resize d = do
-  nodes <- getNodes d
-  nodes' <- MVector.unsafeGrow nodes (MVector.length nodes)
-  writeMutVar (nodesVar d) nodes'
-
-insert :: (PrimMonad m, Ord a) => DisjointSets (PrimState m) a -> a -> m ()
+-- | Insert an element into a 'DisjointSets'.
+--   The element will be inserted as an equivalence class containing only
+--   itself.
+insert :: (PrimMonad m, Ord a)
+  => DisjointSets (PrimState m) a
+  -> a
+  -> m ()
 insert d a = do
   needsResizing <- atMaxCapacity d
   when needsResizing $ do
@@ -67,28 +65,117 @@ insert d a = do
   index <- readMutVar (sizeVar d)
   modifyMutVar' (toIndexVar d) (Map.insert a index)
   modifyMutVar' (fromIndexVar d) (Map.insert index a)
-  readMutVar (nodesVar d) >>= \v -> MVector.write v index (Node { parent = index, size = 1 })
+  readMutVar (nodesVar d) >>= \v -> do
+    MVector.write v index (Node { parent = index, size = 1 })
 
   modifyMutVar' (sizeVar d) (+1)
+{-# inlineable insert #-}
 
-getNodes :: (PrimMonad m) => DisjointSets (PrimState m) a -> m (MVector (PrimState m) Node)
+-- | Returns the minimal element of the equivalence class.
+find :: forall m a. (PrimMonad m, Ord a)
+  => DisjointSets (PrimState m) a
+  -> a
+  -> m (Maybe a)
+find d a = runMaybeT $ do
+  ix <- (MaybeT . findIndex d) =<< MaybeT (getIndex d a)
+  MaybeT (indexToValue d ix)
+{-# inline find #-}
+
+-- | Group the two elements into the same equivalence class.
+--   If one or both of the elements are not present in the 'DisjointSets',
+--   this returns 'False'.
+union :: forall m a. (PrimMonad m, Ord a)
+  => DisjointSets (PrimState m) a
+  -> a
+  -> a
+  -> m Bool
+union d x y
+  -- this check ensures that `x` is the minimal element.
+  | x > y = union d y x
+  | otherwise = fmap isJust $ runMaybeT $ do
+      xRoot <- (MaybeT . findIndex d) =<< MaybeT (getIndex d x)
+      yRoot <- (MaybeT . findIndex d) =<< MaybeT (getIndex d y)
+      if xRoot == yRoot
+        then do
+          pure ()
+        else do
+          Node{size=xRootSize} <- indexToNode d xRoot
+          Node{size=yRootSize} <- indexToNode d yRoot
+          nodes <- getNodes d
+          when (xRootSize < yRootSize) $ do
+            MVector.swap nodes xRoot yRoot
+          MVector.modify nodes (\n -> n { parent = xRoot }) yRoot
+          MVector.modify nodes (\n -> n { size = xRootSize + yRootSize }) xRoot
+{-# inlineable union #-}
+
+-- | Group the elements into the same equivalence class. If any
+--   of the elements are not present in the 'DisjointSets', this returns
+--   'False'.
+unions :: (PrimMonad m, Ord a)
+  => DisjointSets (PrimState m) a
+  -> [a]
+  -> m Bool
+unions d = fmap and . go
+  where
+    go = \case
+      [] -> pure [True]
+      [_] -> pure [True]
+      (x:xs) -> forM xs $ \a -> union d x a
+{-# inline unions #-}
+
+--------------------------------------
+-- Internal types and functions     --
+--------------------------------------
+
+type Index = Int
+
+data Node = Node
+  { parent :: {-# unpack #-} !Index
+  , size :: {-# unpack #-} !Int
+  }
+
+-- get nodes of a DisjointSet
+getNodes :: (PrimMonad m)
+  => DisjointSets (PrimState m) a
+  -> m (MVector (PrimState m) Node)
 getNodes d = readMutVar (nodesVar d)
 
-getIndex :: (PrimMonad m, Ord a) => DisjointSets (PrimState m) a -> a -> m (Maybe Index)
+-- get the index of an element
+getIndex :: (PrimMonad m, Ord a)
+  => DisjointSets (PrimState m) a
+  -> a
+  -> m (Maybe Index)
 getIndex DisjointSets{toIndexVar} a = Map.lookup a <$> readMutVar toIndexVar
 
-getNode :: (PrimMonad m, Ord a) => DisjointSets (PrimState m) a -> a -> m (Maybe Node)
+-- get the node corresponding to an element
+getNode :: (PrimMonad m, Ord a)
+  => DisjointSets (PrimState m) a
+  -> a
+  -> m (Maybe Node)
 getNode d x = do
   mindex <- getIndex d x
   forM mindex $ \index -> indexToNode d index
 
-indexToNode :: (PrimMonad m, Ord a) => DisjointSets (PrimState m) a -> Index -> m Node
+-- get the node at an index
+indexToNode :: (PrimMonad m, Ord a)
+  => DisjointSets (PrimState m) a
+  -> Index
+  -> m Node
 indexToNode d ix = flip MVector.read ix =<< getNodes d
 
-indexToValue :: (PrimMonad m, Ord a) => DisjointSets (PrimState m) a -> Index -> m (Maybe a)
+-- get the value at an index
+indexToValue :: (PrimMonad m, Ord a)
+  => DisjointSets (PrimState m) a
+  -> Index
+  -> m (Maybe a)
 indexToValue DisjointSets{fromIndexVar} ix = Map.lookup ix <$> readMutVar fromIndexVar
 
-findIndex :: forall m a. (PrimMonad m, Ord a) => DisjointSets (PrimState m) a -> Index -> m (Maybe Index)
+-- Like 'find', but takes an index and returns the 'Index' of the found element.
+findIndex :: forall m a. (PrimMonad m, Ord a)
+  => DisjointSets (PrimState m) a
+  -> Index
+  -> m (Maybe Index)
+-- N.B. we use path-halving
 findIndex d ix = runMaybeT $ do
   let go :: Index -> MaybeT m Index
       go !x = do
@@ -103,32 +190,19 @@ findIndex d ix = runMaybeT $ do
             pure x
   go ix
 
-find :: forall m a. (PrimMonad m, Ord a) => DisjointSets (PrimState m) a -> a -> m (Maybe a)
-find d a = runMaybeT $ do
-  ix <- (MaybeT . findIndex d) =<< MaybeT (getIndex d a)
-  MaybeT (indexToValue d ix)
-
-union :: forall m a. (PrimMonad m, Ord a) => DisjointSets (PrimState m) a -> a -> a -> m Bool
-union d x y = fmap isJust $ runMaybeT $ do
-  xRoot <- (MaybeT . findIndex d) =<< MaybeT (getIndex d x)
-  yRoot <- (MaybeT . findIndex d) =<< MaybeT (getIndex d y)
-  if xRoot == yRoot
-    then do
-      pure ()
-    else do
-      Node{size=xRootSize} <- indexToNode d xRoot
-      Node{size=yRootSize} <- indexToNode d yRoot
-      nodes <- getNodes d
-      when (xRootSize < yRootSize) $ do
-        MVector.swap nodes xRoot yRoot
-      MVector.modify nodes (\n -> n { parent = xRoot }) yRoot
-      MVector.modify nodes (\n -> n { size = xRootSize + yRootSize }) xRoot
-
-unions :: (PrimMonad m, Ord a) => DisjointSets (PrimState m) a -> [a] -> m Bool
-unions d = fmap and . go
+-- Are we at max capacity?
+atMaxCapacity :: (PrimMonad m)
+  => DisjointSets (PrimState m) a
+  -> m Bool
+atMaxCapacity d = go <$> readMutVar (sizeVar d) <*> (MVector.length <$> getNodes d)
   where
-    go = \case
-      [] -> pure [True]
-      [_] -> pure [True]
-      (x:xs) -> forM xs $ \a -> union d x a
+    go x y = x + 1 >= y
 
+-- resize operation. we grow the nodes vector by a factor of 2
+resize :: (PrimMonad m)
+  => DisjointSets (PrimState m) a
+  -> m ()
+resize d = do
+  nodes <- getNodes d
+  nodes' <- MVector.unsafeGrow nodes (MVector.length nodes)
+  writeMutVar (nodesVar d) nodes'
