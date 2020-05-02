5cf38be2d5bf5d40a1b5deaf61161d22d778982e
chessai <chessai1996@gmail.com>
2020-05-01 20:21:31 -0700

    use growable vectors

--- src/DisjointSets.hs
+++ src/DisjointSets.hs
@@ -26,6 +26,8 @@ import qualified Data.Map.Strict as Map
 import qualified Data.Vector as Vector
 import qualified Data.Vector.Mutable as MVector
 
+import Prelude hiding (read)
+
 -- | 'DisjointSets' represents a set of disjoint (non-overlapping) sets.
 --
 --   In nearly-constant time, you can find the smallest element of any
@@ -34,20 +36,18 @@ import qualified Data.Vector.Mutable as MVector
 --
 --   The maximum supported size of this structure is roughly 2^32 elements.
 data DisjointSets s a = DisjointSets
-  { sizeVar :: {-# unpack #-} !(MutVar s Int)
-  , toIndexVar :: {-# unpack #-} !(MutVar s (Map a Index))
-  , fromIndexVar :: {-# unpack #-} !(MutVar s (Map Index a))
-  , nodesVar :: {-# unpack #-} !(MutVar s (MVector s Node))
+  { toIndexVar :: {-# unpack #-} !(MutVar s (Map a Index))
+  , fromIndexVar :: {-# unpack #-} !(GrowableVector s a)
+  , nodesVar :: {-# unpack #-} !(GrowableVector s Node)
   }
 
 -- | An 'empty' 'DisjointSets'.
 empty :: (PrimMonad m)
   => m (DisjointSets (PrimState m) a)
 empty = DisjointSets
-  <$> newMutVar 0
-  <*> newMutVar Map.empty
-  <*> newMutVar Map.empty
-  <*> (newMutVar =<< MVector.new 1)
+  <$> newMutVar Map.empty
+  <*> newGrowableVector 1
+  <*> newGrowableVector 1
 {-# inline empty #-}
 
 -- | Insert an element into a 'DisjointSets'.
@@ -58,17 +58,10 @@ insert :: (PrimMonad m, Ord a)
   -> a
   -> m ()
 insert d a = do
-  needsResizing <- atMaxCapacity d
-  when needsResizing $ do
-    resize d
-
-  index <- readMutVar (sizeVar d)
+  index <- sizeOf (nodesVar d)
+  push (Node { parent = index, size = 1 }) (nodesVar d)
   modifyMutVar' (toIndexVar d) (Map.insert a index)
-  modifyMutVar' (fromIndexVar d) (Map.insert index a)
-  readMutVar (nodesVar d) >>= \v -> do
-    MVector.write v index (Node { parent = index, size = 1 })
-
-  modifyMutVar' (sizeVar d) (+1)
+  push a (fromIndexVar d)
 {-# inlineable insert #-}
 
 -- | Returns the minimal element of the equivalence class.
@@ -101,11 +94,11 @@ union d x y
         else do
           Node{size=xRootSize} <- indexToNode d xRoot
           Node{size=yRootSize} <- indexToNode d yRoot
-          nodes <- getNodes d
+          let nodes = nodesVar d
           when (xRootSize < yRootSize) $ do
-            MVector.swap nodes xRoot yRoot
-          MVector.modify nodes (\n -> n { parent = xRoot }) yRoot
-          MVector.modify nodes (\n -> n { size = xRootSize + yRootSize }) xRoot
+            swap nodes xRoot yRoot
+          modify nodes (\n -> n { parent = xRoot }) yRoot
+          modify nodes (\n -> n { size = xRootSize + yRootSize }) xRoot
 {-# inlineable union #-}
 
 -- | Group the elements into the same equivalence class. If any
@@ -118,8 +111,8 @@ unions :: (PrimMonad m, Ord a)
 unions d = fmap and . go
   where
     go = \case
-      [] -> pure [True]
-      [_] -> pure [True]
+      [] -> pure []
+      [_] -> pure []
       (x:xs) -> forM xs $ \a -> union d x a
 {-# inline unions #-}
 
@@ -133,12 +126,6 @@ data Node = Node
   { parent :: {-# unpack #-} !Index
   , size :: {-# unpack #-} !Int
   }
-
--- get nodes of a DisjointSet
-getNodes :: (PrimMonad m)
-  => DisjointSets (PrimState m) a
-  -> m (MVector (PrimState m) Node)
-getNodes d = readMutVar (nodesVar d)
 
 -- get the index of an element
 getIndex :: (PrimMonad m, Ord a)
@@ -161,14 +148,14 @@ indexToNode :: (PrimMonad m, Ord a)
   => DisjointSets (PrimState m) a
   -> Index
   -> m Node
-indexToNode d ix = flip MVector.read ix =<< getNodes d
+indexToNode d ix = read ix (nodesVar d)
 
 -- get the value at an index
 indexToValue :: (PrimMonad m, Ord a)
   => DisjointSets (PrimState m) a
   -> Index
   -> m (Maybe a)
-indexToValue DisjointSets{fromIndexVar} ix = Map.lookup ix <$> readMutVar fromIndexVar
+indexToValue DisjointSets{fromIndexVar} ix = readMaybe ix fromIndexVar
 
 -- Like 'find', but takes an index and returns the 'Index' of the found element.
 findIndex :: forall m a. (PrimMonad m, Ord a)
@@ -183,26 +170,74 @@ findIndex d ix = runMaybeT $ do
         pp <- parent <$> indexToNode d p
         if (p /= x)
           then do
-            nodes <- getNodes d
-            MVector.modify nodes (\n -> n { parent = pp }) p
+            modify (nodesVar d) (\n -> n { parent = pp }) p
             go p
           else do
             pure x
   go ix
 
+-----------------------------------------
+-- Internal data structures            --
+-----------------------------------------
+
+data GrowableVector s a = GrowableVector
+  { sizeVar :: {-# unpack #-} !(MutVar s Int)
+  , vectorVar :: {-# unpack #-} !(MutVar s (MVector s a))
+  }
+
+sizeOf :: PrimMonad m => GrowableVector (PrimState m) a -> m Int
+sizeOf g = readMutVar (sizeVar g)
+
+newGrowableVector :: PrimMonad m => Int -> m (GrowableVector (PrimState m) a)
+newGrowableVector sz = GrowableVector
+  <$> newMutVar 0
+  <*> (newMutVar =<< MVector.new sz)
+
+read :: PrimMonad m => Int -> GrowableVector (PrimState m) a -> m a
+read n g = flip MVector.read n =<< readMutVar (vectorVar g)
+
+readMaybe :: PrimMonad m => Int -> GrowableVector (PrimState m) a -> m (Maybe a)
+readMaybe n g = do
+  sz <- sizeOf g
+  if (n > 0 && n < sz)
+    then Just <$> read n g
+    else pure Nothing
+
+push :: PrimMonad m => a -> GrowableVector (PrimState m) a -> m ()
+push x g = do
+  needsResizing <- atMaxCapacity g
+  when needsResizing (resize g)
+
+  index <- readMutVar (sizeVar g)
+  readMutVar (vectorVar g) >>= \v -> MVector.write v index x
+
+  modifyMutVar' (sizeVar g) (+ 1)
+
+-- We always resize by a factor of 2
+resize :: PrimMonad m => GrowableVector (PrimState m) a -> m ()
+resize (GrowableVector sizeVar var) = do
+  v <- readMutVar var
+  v' <- MVector.unsafeGrow v (MVector.length v)
+  writeMutVar var v'
+
 -- Are we at max capacity?
 atMaxCapacity :: (PrimMonad m)
-  => DisjointSets (PrimState m) a
+  => GrowableVector (PrimState m) a
   -> m Bool
-atMaxCapacity d = go <$> readMutVar (sizeVar d) <*> (MVector.length <$> getNodes d)
+atMaxCapacity g = go <$> readMutVar (sizeVar g) <*> (MVector.length <$> readMutVar (vectorVar g))
   where
     go x y = x + 1 >= y
 
--- resize operation. we grow the nodes vector by a factor of 2
-resize :: (PrimMonad m)
-  => DisjointSets (PrimState m) a
+modify :: PrimMonad m
+  => GrowableVector (PrimState m) a
+  -> (a -> a)
+  -> Int
   -> m ()
-resize d = do
-  nodes <- getNodes d
-  nodes' <- MVector.unsafeGrow nodes (MVector.length nodes)
-  writeMutVar (nodesVar d) nodes'
+modify g f ix = readMutVar (vectorVar g) >>= \v -> MVector.modify v f ix
+
+swap :: PrimMonad m
+  => GrowableVector (PrimState m) a
+  -> Int
+  -> Int
+  -> m ()
+swap g x y = readMutVar (vectorVar g) >>= \v -> MVector.swap v x y
